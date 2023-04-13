751bae2ea65027b09eb9b912d54e139e08b165ee
ryantinder <ryantinder56@gmail.com>
2023-04-13 13:57:49 -0400

    almost working logins

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -2,18 +2,19 @@ import React, { FC } from 'react';
 import { Dimensions, View, StyleSheet } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 
-const{height, width} = Dimensions.get('screen');
+const { height, width } = Dimensions.get('screen');
 
 interface Props {
     placeholder: string;
     onChangeText: (text: string) => void;
     secureTextEntry?: boolean;
+    isPhoneNumber?: boolean;
 }
 
-const Input: FC<Props> = (props) =>  {
-    return(
-        <View style = {styles.container}>
-            <TextInput placeholder= {props.placeholder} secureTextEntry = {props.secureTextEntry || false} onChangeText = {props.onChangeText}/>
+const Input: FC<Props> = (props) => {
+    return (
+        <View style={styles.container}>
+            <TextInput placeholder={props.placeholder} secureTextEntry={props.secureTextEntry || false} onChangeText={props.onChangeText} />
         </View>
     )
 }
@@ -22,7 +23,7 @@ export default Input;
 
 const styles = StyleSheet.create({
     container: {
-        width: width/1.1,
+        width: width / 1.1,
         alignSelf: 'center',
         backgroundColor: '#e3e3e3',
         borderRadius: 5,
---
+++ src/components/signupform.tsx
@@ -0,0 +1,70 @@
+import React, { FC, useState, useRef } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { Button, Input } from "../components";
+import { useSignUp } from "@clerk/clerk-expo";
+
+interface Props {
+    callback: React.Dispatch<React.SetStateAction<boolean>>,
+    emailCallback: React.Dispatch<React.SetStateAction<string>>,
+}
+
+export const SignUpForm: FC<Props> = ({ callback, emailCallback}) => {
+    const[handle, setHandle] = useState("")
+    const[email, setEmail] = useState("")
+    const[password, setPassword] = useState("")
+    const {signUp, setSession, isLoaded} = useSignUp()
+    const [verifyingEmail, setVerifyingEmail] = useState(false)
+
+    const signupHandler = async () => {
+        if (!handle || !email || !password || !isLoaded) {
+            return;
+        }
+        console.log(handle, email, password)
+        try {
+            const completeSignUp = await signUp.create({
+                emailAddress: email,
+                username: handle,
+                password,
+            });
+            callback(true)
+            emailCallback(email)
+            await signUp.prepareEmailAddressVerification({strategy: "email_code"})
+
+            await setSession(completeSignUp.createdSessionId);
+        } catch (err) {
+            // @ts-ignore
+            console.log("Error:> ", JSON.stringify(err));
+        }
+    }
+
+    return (
+        <View style = {styles.container}>
+            <Text>Sign Up</Text>
+            <Input placeholder="Handle" onChangeText={setHandle}/>
+            <Input placeholder="Email" onChangeText={setEmail}/>
+            <Input placeholder="Password" secureTextEntry onChangeText={setPassword}/>
+            <Button title = "Sign Up" onPress={signupHandler}/>
+            <View style = {styles.loginText}>
+                <Text>Already have an account?</Text>
+                <TouchableOpacity onPress={() => {props.navigation.navigate('login')}} style = {{marginHorizontal: 5}}>
+                    <Text style = {{color: 'blue'}}>Login Here</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100%'
+    },
+    loginText:{
+        flexDirection: 'row',
+        marginVertical: 20
+    }
+})
