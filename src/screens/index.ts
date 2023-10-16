c19aaf5b0f1f6d9ae44854db9cacb3c1790642ed
emilyastil <113480110+emilyastil@users.noreply.github.com>
2023-10-16 13:38:32 -0400

    Delete src directory

--- src/screens/index.ts
+++
@@ -1,6 +0,0 @@
-import SignUp from './signup'
-import Login from './login'
-import Home from './home'
-
-export {SignUp, Login, Home}
-
--- src/screens/src/screens/index.ts
+++
@@ -1,7 +0,0 @@
-import SignUp from './signup'
-import Login from './login'
-import Home from './home'
-import Request from './request'
-
-export {SignUp, Login, Home, Request}
-
