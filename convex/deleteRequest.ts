12ba6eff8d5347ccdc1e8325f400d28966722c75
Hayden50 <haydenrieder50@hotmail.com>
2023-12-03 20:07:27 -0500

    Update current deleteRequest function

--- convex/deleteRequest.ts
+++ convex/deleteRequest.ts
@@ -4,10 +4,7 @@ import { mutation } from "./_generated/server";
 This will remove a monetary request from one user to another. This will be called
 for both an accepted and denied request, but will be handled differently in this function.
 */
-export default mutation(async ({ db }, { user_username, friend_username, request_id, accepted }) => {
-    
-    user_username = user_username.toLowerCase();
-    friend_username = friend_username.toLowerCase();
+export default mutation(async ({ db }, { request_id }) => {
 
     const request = await db.query("requests")
     .filter(q => q.eq(q.field("id"), request_id))
@@ -17,13 +14,6 @@ export default mutation(async ({ db }, { user_username, friend_username, request
         return false;
     }
 
-    if (!accepted) {
-        db.delete(request._id);
-        return true;
-    }
-
-    // TODO: Check that the user has the balance needed to pay request
-    // TODO: Reduce the balance of the user here based on amount
     db.delete(request._id);
     return true;
 });
